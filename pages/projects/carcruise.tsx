1659ee03c9aaca28a529ef3781fc19d8c526e2de
Zak Parks <zakparks@gmail.com>
2022-01-05 17:20:56 -0500

    Stub out temp pages

--- pages/projects/carcruise.tsx
+++ pages/projects/carcruise.tsx
@@ -1,1 +1,39 @@
-export {};
+import { NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+import Footer from "../../components/footer";
+import styles from "../../styles/Home.module.css";
+
+const CarCruise: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>asdf</title>
+        <meta name="description" content="asdf" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>Title</h1>
+
+        <p className={styles.description}>asdf</p>
+
+        <div className={styles.grid}>
+          <Link href="#">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              className={`${styles.card} ${styles.card1}`}
+            >
+              <h2>asdf</h2>
+              <p>asdf</p>
+            </a>
+          </Link>
+        </div>
+      </main>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default CarCruise;
